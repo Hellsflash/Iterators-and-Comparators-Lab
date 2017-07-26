19834ed9b3c5223606b85cd4e08cab060cfd967a
Mario Slavov <hellsflash@gmail.com>
2017-07-26 23:33:22 +0300

    Add project files.

---
+++ Library/BookComparator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class BookComparator : IComparer<Book>
+{
+    public int Compare(Book x, Book y)
+    {
+        int result = x.Year.CompareTo(y.Year);
+        if (result == 0)
+        {
+            result = x.Title.CompareTo(y.Title);
+        }
+        return result;
+    }
+}
---
+++ Library/Library.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    private readonly SortedSet<Book> books;
+
+    public Library(params Book[] books)
+    {
+        this.books = new SortedSet<Book>(books, new BookComparator());
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(this.books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+        private int currIndex;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.books = new List<Book>(books);
+            this.Reset();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext() => ++this.currIndex < this.books.Count;
+
+        public void Reset() => this.currIndex = -1;
+
+        public Book Current => this.books[this.currIndex];
+
+        object IEnumerator.Current => this.Current;
+    }
+}
